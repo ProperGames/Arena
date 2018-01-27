5c69557f6c9b2877a9b8b14d74cba12a4c6fd845
Nathan <nirakagames@hotmail.co.uk>
2018-01-27 19:08:52 +0000

    Added unit conversion tool
    + Added Common.Tools.UnitConversions

---
+++ Common/Source/Tools/UnitConversions.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Tools
+{
+    public static class UnitConversions
+    {
+        public static long KilobytesInBytes(int iKilobytes)
+        {
+            return iKilobytes * 1000;
+        }
+
+        public static long KibibytesInBytes(int iKibibytes)
+        {
+            return iKibibytes * 1024;
+        }
+    }
+}
